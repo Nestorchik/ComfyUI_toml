4314f120df3c5b78be163744809ecb9de97b6d18
NStor <74113797+Nestorchik@users.noreply.github.com>
2025-07-13 21:37:12 +0300

    Update install_ComfyUI.bat

--- install_ComfyUI.bat
+++ install_ComfyUI.bat
@@ -1,4 +1,5 @@
 @echo off && setlocal && cd /D %CD% && powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+uv python install 3.12
 git clone https://github.com/comfyanonymous/ComfyUI.git
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git "%CD%\ComfyUI\custom_nodes\ComfyUI-Manager"
 run.bat
